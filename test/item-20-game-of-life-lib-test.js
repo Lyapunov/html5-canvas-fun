1657d68057bb7b3777fea18e45e74904c4ad66de
Lyapunov <vaskorichard@gmail.com>
2016-10-28 13:55:20 +0200

    Using proper names instead of Gizmo.

--- test/item-20-game-of-life-lib-test.js
+++ test/item-20-game-of-life-lib-test.js
@@ -3,7 +3,7 @@ var assert = buster.referee.assert;
 
 var subj = require("../item-20-game-of-life-lib");
 
-buster.testCase("Gizmo", {
+buster.testCase("Game of life lib", {
    "uses the right color" : function() {
       assert.equals(subj.color(), "purple");
    }
