3d66d4fd35d7c0195d8ea27c9f30cccb39675aef
Lyapunov <vaskorichard@gmail.com>
2016-10-28 13:53:49 +0200

    Removing useless logging.

--- test/gizmo-test.js
+++ test/gizmo-test.js
@@ -2,7 +2,6 @@ var buster = require("buster");
 var assert = buster.referee.assert;
 
 var subj = require("../item-20-game-of-life-lib");
-console.log(subj);
 
 buster.testCase("Gizmo", {
    "uses the right color" : function() {
