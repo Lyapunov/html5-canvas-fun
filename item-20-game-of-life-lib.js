91feba0ae7b0615fe920184d2674f014e49ebc7b
Lyapunov <vaskorichard@gmail.com>
2016-11-02 13:04:24 +0100

    TDD, round 1. First test case for GameBoard, then implementing it in the lib.

--- item-20-game-of-life-lib.js
+++ item-20-game-of-life-lib.js
@@ -1,5 +1,15 @@
 var color = function() {
    return "purple";
-};
+}
+
+var GameBoard = function() {
+   this.addCell = function (x,y) {
+   };
+   this.tick = function() {}
+   this.getCell = function (x,y) {
+      return 0;
+   };
+}
 
 module.exports.color = color;
+module.exports.GameBoard = GameBoard;
--- test/item-20-game-of-life-lib-test.js
+++ test/item-20-game-of-life-lib-test.js
@@ -6,5 +6,11 @@ var subj = require("../item-20-game-of-life-lib");
 buster.testCase("Game of life lib", {
    "uses the right color" : function() {
       assert.equals(subj.color(), "purple");
+   },
+   "lonely cell dies" : function() {
+      var game = new subj.GameBoard();
+      game.addCell(1,1);
+      game.tick();
+      assert.equals(game.getCell(0,0), 0);
    }
 });
