e038abe61379456d0afd038faa72c897118970f0
Lyapunov <vaskorichard@gmail.com>
2016-11-03 11:27:07 +0100

    Removing useless fake test.

--- test/item-20-game-of-life-lib-test.js
+++ test/item-20-game-of-life-lib-test.js
@@ -4,9 +4,6 @@ var assert = buster.referee.assert;
 var subj = require("../item-20-game-of-life-lib");
 
 buster.testCase("Game of life lib", {
-   "uses the right color" : function() {
-      assert.equals(subj.color(), "purple");
-   },
    "lonely cell dies" : function() {
       var game = new subj.GameBoard();
       game.addCell(1,1);
