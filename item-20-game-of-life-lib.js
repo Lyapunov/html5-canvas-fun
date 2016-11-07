6f48425bbdea51864ae54929c676539c291bcb93
Lyapunov <vaskorichard@gmail.com>
2016-11-07 12:54:39 +0100

    Adding code, now tests are green.

--- item-20-game-of-life-lib.js
+++ item-20-game-of-life-lib.js
@@ -3,14 +3,39 @@ var color = function() {
 }
 
 var GameBoard = function() {
+   var self = this;
    this.cells=[];
    this.addCell = function (cx,cy) {
       this.cells.push({x:cx,y:cy});
    };
-   this.tick = function() {}
-   this.getCell = function (x,y) {
-      console.log(this.cells.length > 2);
-      return ( this.cells.length > 2 );
+   this.numberOfNeighbours = function (pxy) {
+      var num = 0;
+      this.cells.forEach( function( xy ) {
+         if ( Math.abs(xy.x - pxy.x) <= 1
+              && Math.abs(xy.y - pxy.y) <= 1 ) {
+            ++num;
+         }
+      } );
+      return num;
+   };
+   this.tick = function() {
+      var newCells=[];
+      this.cells.forEach( function( xy ) {
+         var non = self.numberOfNeighbours( xy )-1;
+         if ( non == 2 || non == 3 ) {
+            newCells.push( xy ); 
+         }
+      } );
+      this.cells = newCells;
+   };
+   this.getCell = function (cx,cy) {
+      var num = false;
+      this.cells.forEach( function( xy ) {
+         if ( xy.x == cx && xy.y == cy ) {
+            num = true;
+         }
+      } );
+      return num;
    };
 }
 
