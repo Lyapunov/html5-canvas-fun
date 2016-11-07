3a41947d0778d64cfeebb2c26e477b89b5dc0d80
Lyapunov <vaskorichard@gmail.com>
2016-11-07 12:58:49 +0100

    Refactored.

--- test/item-20-game-of-life-lib-test.js
+++ test/item-20-game-of-life-lib-test.js
@@ -4,26 +4,26 @@ var assert = buster.referee.assert;
 var subj = require("../item-20-game-of-life-lib");
 
 buster.testCase("Game of life lib", {
+   setUp: function() {
+      this.game = new subj.GameBoard();
+   },
    "lonely cell dies" : function() {
-      var game = new subj.GameBoard();
-      game.addCell(1,1);
-      game.tick();
-      assert.equals(game.getCell(0,0), false);
+      this.game.addCell(1,1);
+      this.game.tick();
+      assert.equals(this.game.getCell(0,0), false);
    },
    "middle cell survives" : function() {
-      var game = new subj.GameBoard();
-      game.addCell(1,1);
-      game.addCell(2,1);
-      game.addCell(3,1);
-      game.tick();
-      assert.equals(game.getCell(2,1), true);
+      this.game.addCell(1,1);
+      this.game.addCell(2,1);
+      this.game.addCell(3,1);
+      this.game.tick();
+      assert.equals(this.game.getCell(2,1), true);
    },
    "outer cell dies" : function() {
-      var game = new subj.GameBoard();
-      game.addCell(1,1);
-      game.addCell(2,1);
-      game.addCell(3,1);
-      game.tick();
-      assert.equals(game.getCell(3,1), false);
+      this.game.addCell(1,1);
+      this.game.addCell(2,1);
+      this.game.addCell(3,1);
+      this.game.tick();
+      assert.equals(this.game.getCell(3,1), false);
    }
 });
