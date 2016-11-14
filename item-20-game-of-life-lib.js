346a4db91310049d57fa0b86bab31bea09758570
Lyapunov <vaskorichard@gmail.com>
2016-11-14 13:42:55 +0100

    Adding new test case and implementation.

--- item-20-game-of-life-lib.js
+++ item-20-game-of-life-lib.js
@@ -12,7 +12,8 @@ var GameBoard = function() {
       var num = 0;
       this.cells.forEach( function( xy ) {
          if ( Math.abs(xy.x - pxy.x) <= 1
-              && Math.abs(xy.y - pxy.y) <= 1 ) {
+              && Math.abs(xy.y - pxy.y) <= 1
+              && !( xy.x == pxy.x && xy.y == pxy.y ) ) {
             ++num;
          }
       } );
@@ -21,9 +22,22 @@ var GameBoard = function() {
    this.tick = function() {
       var newCells=[];
       this.cells.forEach( function( xy ) {
-         var non = self.numberOfNeighbours( xy )-1;
+         var non = self.numberOfNeighbours( xy );
          if ( non == 2 || non == 3 ) {
             newCells.push( xy ); 
+         }
+      } );
+      this.cells.forEach( function( xy ) {
+         for ( var i = -1; i <= 1; ++i ) {
+            for ( var j = -1; j <= 1; ++j ) {
+               if ( i == 0 && j == 0 ) {
+                  continue;
+               }
+               var nxy = {x:xy.x+i,y:xy.y+j};
+               if ( self.numberOfNeighbours( nxy ) == 3 ) {
+                  newCells.push( nxy );
+               }
+            }
          }
       } );
       this.cells = newCells;
