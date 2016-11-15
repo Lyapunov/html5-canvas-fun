4f2281ff5b14ff40de8155646e0e06240ddf1e9d
Lyapunov <vaskorichard@gmail.com>
2016-11-15 13:52:25 +0100

    Adding code to visualize the game of life.

--- item-20-game-of-life.js
+++ item-20-game-of-life.js
@@ -6,14 +6,32 @@ var context      = mainItem.getContext('2d');
 
 var screenWidth  = mainItem.width  = window.innerWidth  - 20;
 var screenHeight = mainItem.height = window.innerHeight - 20;
-var screenScale  = Math.min( screenWidth, screenHeight ) / 2;
+var screenScale  = Math.min( screenWidth, screenHeight );
+
+var board;
+var tiles = 25;
 
 var fps = 25;
 var ANIMATION;
+var noColor = "blue";
+var yesColor = "red";
 
 function drawBackground() {
    context.fillStyle = color();
-   context.fillRect(0,0,screenWidth,screenHeight);
+
+   if ( board !== 'undefined' ) {
+      for ( var x = 0; x < tiles; ++ x ) {
+         for ( var y = 0; y < tiles; ++ y ) {
+            var ax = ( x * screenScale ) / tiles;
+            var ay = ( y * screenScale ) / tiles;
+            var bx = ( (x+1) * screenScale ) / tiles;
+            var by = ( (y+1) * screenScale ) / tiles;
+            context.fillStyle = board.getCell(x,y) ? yesColor : noColor;
+            var shift = (screenWidth - screenScale) / 2;
+            context.fillRect(ax+shift,ay,bx+shift,by);
+         }
+      }
+   }
 }
 
 // common functions 
@@ -21,13 +39,26 @@ function drawScene() {
    setTimeout( function() {
       drawBackground();
       ANIMATION = requestAnimationFrame(drawScene);
+      if ( board !== 'undefined' ) {
+         board.tick();
+      }
    }, 1000 / fps );
 }
 
 function initCanvas() {
    screenWidth  = mainItem.width  = window.innerWidth  - 20;
    screenHeight = mainItem.height = window.innerHeight - 20;
-   screenScale  = Math.min( screenWidth, screenHeight ) / 2;
+   screenScale  = Math.min( screenWidth, screenHeight );
+
+   context.fillStyle = noColor;
+   context.fillRect(0,0,screenWidth,screenHeight);
+
+   board = new GameBoard;
+   board.addCell(1,0);
+   board.addCell(2,1);
+   board.addCell(0,2);
+   board.addCell(1,2);
+   board.addCell(2,2);
 
    // starting the engine only if it is not running
    if ( !ANIMATION ) {
